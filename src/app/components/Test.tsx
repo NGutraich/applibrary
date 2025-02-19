b2dd1a6fa61d15404a3d9d326bdf41001beaf59e
NGutraich <ndgutraich@gmail.com>
2025-02-19 10:03:23 -0300

    Project setup completed with shadcn installed

---
+++ src/app/components/Test.tsx
@@ -0,0 +1,8 @@
+// src/components/Test.tsx
+export default function Test() {
+    return (
+      <div className="bg-background text-foreground p-4">
+        Tailwind is working! 🚀
+      </div>
+    );
+  }
